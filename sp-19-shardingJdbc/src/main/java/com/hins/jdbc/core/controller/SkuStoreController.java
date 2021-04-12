850327ce2e95b50faf3ef065ad8786cb5481c84f
1805455228@qq.com <1805455228@qq.com>
2021-04-12 15:50:45 +0800

    mybatisplus 多数据源

--- sp-19-shardingJdbc/src/main/java/com/hins/jdbc/core/controller/SkuStoreController.java
+++ sp-19-shardingJdbc/src/main/java/com/hins/jdbc/core/controller/SkuStoreController.java
@@ -8,6 +8,8 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hins.jdbc.config.mybatisplus.ValueTableNameParser;
 import com.hins.jdbc.core.entity.SkuStore;
 import com.hins.jdbc.core.service.ISkuStoreService;
+import com.hins.jdbc.order.entity.OrderItem;
+import com.hins.jdbc.order.service.IOrderItemService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,6 +35,9 @@ public class SkuStoreController {
 
     @Autowired
     private ISkuStoreService skuStoreService;
+
+    @Autowired
+    private IOrderItemService orderItemService;
 
     @GetMapping("/list")
     public Object getStoreSkuList(@RequestParam String storeId){
@@ -76,20 +81,49 @@ public class SkuStoreController {
     public String addStoreSku(@RequestParam String storeId){
 
         try {
-            SkuStore entity = new SkuStore();
-            entity.setSkuId(UUID.randomUUID().toString().replace("-",""));
-            entity.setStoreId(storeId);
-            entity.setPrice(new BigDecimal(30.00));
-            entity.setMbrPrice(new BigDecimal(80.00));
-            entity.setStock(200);
-            entity.setSkuStatus(0);
-            entity.setUpdateTime(new Date());
+            SkuStore entity2 = new SkuStore();
+            entity2.setSkuId(UUID.randomUUID().toString().replace("-",""));
+            entity2.setStoreId(storeId);
+            entity2.setPrice(new BigDecimal(30.00));
+            entity2.setMbrPrice(new BigDecimal(80.00));
+            entity2.setStock(200);
+            entity2.setSkuStatus(0);
+            entity2.setUpdateTime(new Date());
             ValueTableNameParser.setThreadLocalValue(storeId);
-            skuStoreService.save(entity);
-            return "添加成功"+storeId;
+            skuStoreService.save(entity2);
+
+
+
+            //另外一个数据源的操作
+
+            Integer num = (int)(1+Math.random()*(1000-1+1));
+            OrderItem entity = new OrderItem();
+            Long time = System.currentTimeMillis();
+            entity.setOrderId(String.valueOf(time));
+            entity.setMemberId(num.longValue());
+            entity.setSpuName("spu商品名称-"+num);
+            entity.setSpuId(UUID.randomUUID().toString().replace("-",""));
+            entity.setSkuId(UUID.randomUUID().toString().replace("-",""));
+            entity.setSkuName("sku商品名称-"+num);
+            entity.setQuantity(10);
+            entity.setRealAmount(new BigDecimal(30.00));
+            entity.setPrice(new BigDecimal(30.00));
+            entity.setPromotionAmount(new BigDecimal(10.00));
+            entity.setCouponAmount(new BigDecimal(20.00));
+            entity.setBarCode(String.valueOf(time));
+
+            entity.setCouponCode(String.valueOf(time));
+            entity.setFirstCategoryId(String.valueOf(time));
+            entity.setStateDescribe("nnnnnnn-des");
+            entity.setUpdateTime(new Date());
+
+            orderItemService.save(entity);
+            return "添加成功";
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+
 
 
         return "添加异常，表不存在"+storeId;
