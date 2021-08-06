f752777934ece6dd94831df343ac3d884c8274bc
1805455228@qq.com <1805455228@qq.com>
2021-08-06 14:57:57 +0800

    websocket例子前端页面

---
+++ sp-21-websocket2-redis/src/main/java/com/hins/sp21websocket/config/WebMvcConfig.java
@@ -0,0 +1,43 @@
+package com.hins.sp21websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @author : chenqixuan
+ * @date : 2021/8/6
+ */
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+
+
+//    public CorsWebFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+//        source.registerCorsConfiguration("/pintea-order/ws/**", buildConfig());
+//        return new CorsWebFilter(source);
+//    }
+//
+//    private CorsConfiguration buildConfig() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.addAllowedOrigin("*");
+//        corsConfiguration.addAllowedHeader("*");
+//        corsConfiguration.addAllowedMethod("*");
+//        corsConfiguration.setAllowCredentials(true);
+//        return corsConfiguration;
+//    }
+}
+
