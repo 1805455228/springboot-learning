99da15ed7840dd513e91bdd761c6bff429712034
1805455228@qq.com <1805455228@qq.com>
2021-10-28 13:54:38 +0800

    视频截图

--- sp-08-netty/src/main/java/videoDownload/VideoCoverHandel.java
+++ sp-08-netty/src/main/java/videoDownload/VideoCoverHandel.java
@@ -5,4 +5,13 @@ package videoDownload;
  * @date : 2021/10/28
  */
 public class VideoCoverHandel {
+
+
+    public static void main(String[] args) {
+        //todo 遍历文件夹视频
+
+        //todo 截取视频封面
+
+        //todo 把数据整理入库
+    }
 }
