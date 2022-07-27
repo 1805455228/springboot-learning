7ce35299f259f9a2f850a6ad04b1574b562a3423
kenhinschan <1805455228@qq.com>
2022-07-27 20:14:40 +0800

    poi sxssfworkbook xlsx oom

--- sp-01-hello/src/main/java/com/hins/sp01hello/excel/PoiExeelSXSSFTest.java
+++ sp-01-hello/src/main/java/com/hins/sp01hello/excel/PoiExeelSXSSFTest.java
@@ -1,9 +1,8 @@
 package com.hins.sp01hello.excel;
 
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.DateUtil;
-import org.apache.poi.ss.usermodel.HorizontalAlignment;
-import org.apache.poi.ss.usermodel.VerticalAlignment;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.*;
 import org.springframework.util.StopWatch;
 
@@ -22,14 +21,15 @@ public class PoiExeelSXSSFTest {
 
 
     public static void main(String[] args) {
-        //createExcelData();
-        createExcelData2();
-        createExcelData3();
+
+        createExcelData_bigFile();
+        //使用xssfworkbook 会内存溢出
+        //createExcelData_oom();
 
 
     }
 
-    private static void createExcelData3() {
+    private static void createExcelData_bigFile() {
         HashMap<String, String> titleMap = new HashMap();
         titleMap.put("姓名", "name");
         titleMap.put("年龄", "age");
@@ -40,7 +40,7 @@ public class PoiExeelSXSSFTest {
         titleMap.put("金额", "price");
 
         List<Map<String,Object>> list = new ArrayList<>();
-        for(int i=0;i<5000;i++){
+        for(int i=0;i<500000;i++){
             HashMap<String, Object> hashMap = new HashMap();
             hashMap.put("name", "张三"+i);
             hashMap.put("age", i);
@@ -55,19 +55,21 @@ public class PoiExeelSXSSFTest {
         StopWatch sw = new StopWatch();
 //        String exceltemplate1 = "/Users/chenqixuan/Documents/IntelliJIDEAWorkspace/springboot-learning/sp-01-hello/src/main/java/com/hins/sp01hello/excel/template1.xlsx";
         String exceltemplate2 = "/Users/chenqixuan/Documents/IntelliJIDEAWorkspace/springboot-learning/sp-01-hello/src/main/java/com/hins/sp01hello/excel/template2.xlsx";
-        XSSFWorkbook workbook = null;
+        XSSFWorkbook workbookXlsx = null;
+        SXSSFWorkbook workbook = null;
         try {
             sw.start();
             FileInputStream inputStream = new FileInputStream(exceltemplate2);
-            workbook = new XSSFWorkbook(inputStream);
+            workbookXlsx = new XSSFWorkbook(inputStream);
+            workbook = new SXSSFWorkbook(workbookXlsx,500);
             // 获取sheet
-            XSSFSheet sheet = workbook.getSheetAt(0);
-            sheet.setForceFormulaRecalculation(true);
+            // 使用XSSFSheet（错误写法，数据量大会内存溢出）
+            //Sheet sheet = workbook.getXSSFWorkbook().getSheetAt(0);
+            // 应使用SXSSFSheet（避免内存溢出）(推荐)
+            SXSSFSheet sheet = workbook.getSheetAt(0);
+            //sheet.setForceFormulaRecalculation(true);
 
-            XSSFRow titleRow = sheet.getRow(0);
-            if(null == titleRow){
-                titleRow = sheet.createRow(0);
-            }
+            Row titleRow = workbook.getXSSFWorkbook().getSheetAt(0).getRow(0);
             //记录标题行单元格
             HashMap<Integer, String> titleIndexMap = new HashMap();
             short startCell = titleRow.getFirstCellNum();
@@ -81,17 +83,17 @@ public class PoiExeelSXSSFTest {
             //填充数据
             for (int i = 0; i < list.size() ; i++) {
                 //创建内容行
-                XSSFRow dataRow = sheet.getRow(j+i );
+                Row dataRow = sheet.getRow(j+i );
                 if(null == dataRow){
                     dataRow = sheet.createRow(j+i);
                 }
                 //创建内容行单元格,填充数据
                 for(int n=startCell;n<endCell;n++){
-                    XSSFCell titleCell = titleRow.getCell(n);
+                    Cell titleCell = titleRow.getCell(n);
                     CellType cellType = titleCell.getCellType();
-                    XSSFCell cell = dataRow.createCell(n);
+                    Cell cell = dataRow.createCell(n);
                     cell.setCellType(cellType);
-                    XSSFCellStyle cellStyle = cell.getCellStyle();
+                    CellStyle cellStyle = cell.getCellStyle();
                     //设置格式
                     cellStyle.setAlignment(HorizontalAlignment.CENTER);//设置水平居中
                     cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);//设置垂直居中
@@ -130,6 +132,8 @@ public class PoiExeelSXSSFTest {
             FileOutputStream out = new FileOutputStream("/Users/chenqixuan/Downloads/"+newFileName);
             workbook.write(out);
             out.close();
+            workbook.close();
+            workbook.dispose();
             sw.stop();
             System.out.println("耗时2："+sw.getTotalTimeSeconds());
         } catch (IOException e) {
@@ -137,44 +141,49 @@ public class PoiExeelSXSSFTest {
         }
     }
 
-    private static void createExcelData2() {
+    /**
+     * 默认jvm内存配置20万 内存溢出oom
+     */
+    private static void createExcelData_oom() {
         HashMap<String, String> titleMap = new HashMap();
         titleMap.put("姓名", "name");
         titleMap.put("年龄", "age");
         titleMap.put("性别", "sex");
         titleMap.put("出生年月日","createDate");
         titleMap.put("身价", "singlePrice");
+        titleMap.put("订单编号", "orderNo");
+        titleMap.put("金额", "price");
 
         List<Map<String,Object>> list = new ArrayList<>();
-        HashMap<String, Object> hashMap = new HashMap();
-        hashMap.put("name", "张三");
-        hashMap.put("age", 18);
-        hashMap.put("sex", "女");
-        hashMap.put("createDate", new Date());
-        hashMap.put("singlePrice", 12.00F);
-        list.add(hashMap);
-        HashMap<String, Object> hashMap1 = new HashMap();
-        hashMap1.put("name", "李四");
-        hashMap1.put("age", 16);
-        hashMap1.put("sex", "男");
-        hashMap1.put("createDate", new Date());
-        hashMap1.put("singlePrice", 123.77);
-        list.add(hashMap1);
+        for(int i=0;i<200000;i++){
+            HashMap<String, Object> hashMap = new HashMap();
+            hashMap.put("name", "张三"+i);
+            hashMap.put("age", i);
+            hashMap.put("sex", "女"+i);
+            hashMap.put("createDate", new Date());
+            hashMap.put("singlePrice", 12.00F+i);
+            hashMap.put("price", 88.58+i);
+            hashMap.put("orderNo", "99907"+i);
+            list.add(hashMap);
+        }
 
-        String exceltemplate1 = "/Users/chenqixuan/Documents/IntelliJIDEAWorkspace/springboot-learning/sp-01-hello/src/main/java/com/hins/sp01hello/excel/template1.xlsx";
+        StopWatch sw = new StopWatch();
+//        String exceltemplate1 = "/Users/chenqixuan/Documents/IntelliJIDEAWorkspace/springboot-learning/sp-01-hello/src/main/java/com/hins/sp01hello/excel/template1.xlsx";
         String exceltemplate2 = "/Users/chenqixuan/Documents/IntelliJIDEAWorkspace/springboot-learning/sp-01-hello/src/main/java/com/hins/sp01hello/excel/template2.xlsx";
-        XSSFWorkbook workbook = null;
+        XSSFWorkbook workbookXlsx = null;
+        SXSSFWorkbook workbook = null;
         try {
-            FileInputStream inputStream = new FileInputStream(exceltemplate1);
-            workbook = new XSSFWorkbook(inputStream);
-            // 获取sheet
-            XSSFSheet sheet = workbook.getSheetAt(0);
+            sw.start();
+            FileInputStream inputStream = new FileInputStream(exceltemplate2);
+            workbookXlsx = new XSSFWorkbook(inputStream);
+            workbook = new SXSSFWorkbook(workbookXlsx,1000);
+            // 使用XSSFSheet（数据量大会内存溢出）
+            Sheet sheet = workbook.getXSSFWorkbook().getSheetAt(0);
+            // 应使用SXSSFSheet（避免内存溢出）(推荐)
+            //SXSSFSheet sheet = workbook.getSheetAt(0);
             sheet.setForceFormulaRecalculation(true);
 
-            XSSFRow titleRow = sheet.getRow(0);
-            if(null == titleRow){
-                titleRow = sheet.createRow(0);
-            }
+            Row titleRow = workbook.getXSSFWorkbook().getSheetAt(0).getRow(0);
             //记录标题行单元格
             HashMap<Integer, String> titleIndexMap = new HashMap();
             short startCell = titleRow.getFirstCellNum();
@@ -188,17 +197,17 @@ public class PoiExeelSXSSFTest {
             //填充数据
             for (int i = 0; i < list.size() ; i++) {
                 //创建内容行
-                XSSFRow dataRow = sheet.getRow(j+i );
+                Row dataRow = sheet.getRow(j+i );
                 if(null == dataRow){
                     dataRow = sheet.createRow(j+i);
                 }
                 //创建内容行单元格,填充数据
                 for(int n=startCell;n<endCell;n++){
-                    XSSFCell titleCell = titleRow.getCell(n);
+                    Cell titleCell = titleRow.getCell(n);
                     CellType cellType = titleCell.getCellType();
-                    XSSFCell cell = dataRow.createCell(n);
+                    Cell cell = dataRow.createCell(n);
                     cell.setCellType(cellType);
-                    XSSFCellStyle cellStyle = cell.getCellStyle();
+                    CellStyle cellStyle = cell.getCellStyle();
                     //设置格式
                     cellStyle.setAlignment(HorizontalAlignment.CENTER);//设置水平居中
                     cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);//设置垂直居中
@@ -226,143 +235,25 @@ public class PoiExeelSXSSFTest {
                     } else {
                         cell.setCellValue(String.valueOf(fieldValue));
                     }
-
                 }
             }
             String newFileName = UUID.randomUUID().toString() + "_" + "送货单" + ".xlsx";
+            sw.stop();
+            System.out.println("耗时1："+sw.getTotalTimeSeconds());
 
             //修改模板内容导出新模板
+            sw.start();
             FileOutputStream out = new FileOutputStream("/Users/chenqixuan/Downloads/"+newFileName);
             workbook.write(out);
             out.close();
+            workbook.close();
+            workbook.dispose();
+            sw.stop();
+            System.out.println("耗时2："+sw.getTotalTimeSeconds());
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
 
-
-    private static void createExcelData() {
-        HashMap<String, String> titleMap = new HashMap();
-        titleMap.put("姓名", "name");
-        titleMap.put("年龄", "age");
-        titleMap.put("性别", "sex");
-        titleMap.put("出现日期","createDate");
-        titleMap.put("身价", "singlePrice");
-
-        List<Map<String,Object>> list = new ArrayList<>();
-        HashMap<String, Object> hashMap = new HashMap();
-        hashMap.put("name", "张三");
-        hashMap.put("age", 18);
-        hashMap.put("sex", "女");
-        hashMap.put("createDate", new Date());
-        hashMap.put("singlePrice", 12.00F);
-        list.add(hashMap);
-        HashMap<String, Object> hashMap1 = new HashMap();
-        hashMap1.put("name", "李四");
-        hashMap1.put("age", 16);
-        hashMap1.put("sex", "男");
-        hashMap1.put("createDate", new Date());
-        hashMap1.put("singlePrice", 123.77);
-        list.add(hashMap1);
-
-        String exceltemplate1 = "/Users/chenqixuan/Documents/IntelliJIDEAWorkspace/springboot-learning/sp-01-hello/src/main/java/com/hins/sp01hello/excel/template1.xlsx";
-        String exceltemplate2 = "/Users/chenqixuan/Documents/IntelliJIDEAWorkspace/springboot-learning/sp-01-hello/src/main/java/com/hins/sp01hello/excel/template2.xlsx";
-        XSSFWorkbook workbook = null;
-        try {
-            FileInputStream inputStream = new FileInputStream(exceltemplate1);
-            workbook = new XSSFWorkbook(inputStream);
-            // 获取sheet
-            XSSFSheet sheet = workbook.getSheetAt(0);
-            sheet.setForceFormulaRecalculation(true);
-
-            XSSFRow titleRow = sheet.getRow(0);
-            if(null == titleRow){
-                titleRow = sheet.createRow(0);
-            }
-            //记录标题行单元格
-            HashMap<Integer, String> titleIndexMap = new HashMap();
-            short startCell = titleRow.getFirstCellNum();
-            short endCell = titleRow.getLastCellNum();
-            for(int m= startCell;m<endCell;m++){
-                titleIndexMap.put(m,titleRow.getCell(m).getStringCellValue());
-            }
-
-
-            int j = 1;//去除标题行
-            //填充数据
-            for (int i = 0; i < list.size() ; i++) {
-                //创建内容行
-                XSSFRow dataRow = sheet.getRow(j+i );
-                if(null == dataRow){
-                    dataRow = sheet.createRow(j+i);
-                }
-                //创建内容行单元格,填充数据
-                for(int n=startCell;n<endCell;n++){
-                    XSSFCell titleCell = titleRow.getCell(n);
-                    CellType cellType = titleCell.getCellType();
-
-                    XSSFCell cell = dataRow.createCell(n);
-                    cell.setCellType(cellType);
-
-                    String titleKey = titleIndexMap.get(n);//根据下标获取标题文本
-                    String key = titleMap.get(titleKey);//根据标题文本货物属性名
-                    Object fieldValue = list.get(i).get(key);//根据属性获取value
-                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                    //cell.setCellValue(String.valueOf(obj));
-                    if (fieldValue instanceof Integer) {
-                        cell.setCellValue((Integer) fieldValue);
-                    } else if (fieldValue instanceof Long) {
-                        cell.setCellValue((Long) fieldValue);
-                    } else if (fieldValue instanceof Double) {
-                        cell.setCellValue((Double) fieldValue);
-                    } else if (fieldValue instanceof Double) {
-                        cell.setCellValue((Double) fieldValue);
-                    } else if (fieldValue instanceof Boolean) {
-                        cell.setCellValue((Boolean) fieldValue);
-                    } else if (fieldValue instanceof Date) {
-                        Date date = (Date) fieldValue;
-                        String dateStr = formatter.format(date);
-                        cell.setCellValue(dateStr);
-                    } else {
-                        cell.setCellValue(String.valueOf(fieldValue));
-                    }
-
-                }
-            }
-            String newFileName = UUID.randomUUID().toString() + "_" + "送货单" + ".xlsx";
-
-            //修改模板内容导出新模板
-            FileOutputStream out = new FileOutputStream("/Users/chenqixuan/Downloads/"+newFileName);
-            workbook.write(out);
-            out.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    //读取excel数据的单元格数据类型转换
-    private String getCellValue(XSSFCell cell){
-        String cellValue = "";
-        DecimalFormat df = new DecimalFormat("#.##");
-        if(cell.getCellType() == CellType.NUMERIC){
-            if (DateUtil.isCellDateFormatted(cell)){
-                Date date = cell.getDateCellValue();
-                //格式转换
-                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-                cellValue = sdf.format(date);
-            }else {
-                cellValue = df.format(cell.getNumericCellValue()).toString();
-            }
-        }else if (cell.getCellType() == CellType.STRING){
-            cellValue = cell.getStringCellValue();
-        }else if (cell.getCellType() == CellType.FORMULA){
-            cellValue = cell.getCellFormula();
-        }else if (cell.getCellType() == CellType.BLANK){
-            cellValue = "";
-        }else if (cell.getCellType() == CellType.BOOLEAN){
-            cellValue = String.valueOf(cell.getBooleanCellValue()).trim();
-        }
-        return cellValue;
-    }
 }
