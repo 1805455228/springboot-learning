4f913c0ec5f148e0c8215aa8f182398e075c3e41
qixuan <1805455228@qq.com>
2022-11-18 15:10:08 +0800

    手写ioc

---
+++ sp-24-myspring-ioc/src/main/java/com/hins/sp24/sp24myioc/Sp24MyIocApplication.java
@@ -0,0 +1,13 @@
+package com.hins.sp24.sp24myioc;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class Sp24MyIocApplication {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(Sp23mongodbApplication.class, args);
+    }
+
+}
